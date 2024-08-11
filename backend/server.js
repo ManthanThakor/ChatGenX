265c9d6f4f4a5a4f1a4b0565f85d527a00672f85
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-11 16:37:45 +0530

    MERN - ChatGenX Project - Backend - 9

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,18 @@
-// const path = require("path");
-// const dotenv = require("dotenv");
-// dotenv.config({ path: path.resolve(__dirname, ".env") });
+const path = require("path");
+const dotenv = require("dotenv");
+dotenv.config({ path: path.resolve(__dirname, ".env") });
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+//! Middleware
+
+app.use(express.json());
+
+//! Start server
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
