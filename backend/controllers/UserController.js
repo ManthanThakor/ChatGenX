3a08fb0d9b7a1ea9a02baa21a84083e4265dac3a
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-12 14:52:57 +0530

    MERN - ChatGenX Project - Backend - 15

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,10 +1,38 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
 //--------- Registration ---------
 
 const register = async (req, res) => {
-  res.json({
-    status: true,
-    message: "Registration was successful",
-  });
+  try {
+    const { username, email, password } = req.body;
+
+    //! Validate
+    if (!username || !email || !password) {
+      return res
+        .status(400)
+        .json({ message: "Please all fields are required" });
+    }
+    // Check if user already exists
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ message: "Email already exists" });
+    }
+    // Hash the password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    // Create a new user
+    const user = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    // Save the user
+    await user.save();
+    // Return success message
+    res.json({
+      status: true,
+      message: "Registration was successful",
+    });
+  } catch (err) {}
 };
 //--------- Login ---------
 //--------- Logout ---------
