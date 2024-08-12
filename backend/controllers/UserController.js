7355f27eba5ac231a851654e67c14c1f848e664e
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-12 15:00:36 +0530

    MERN - ChatGenX Project - Backend - 16

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,39 +1,53 @@
+const asyncHandler = require("express-async-handler"); // Import asyncHandler
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+
 //--------- Registration ---------
 
-const register = async (req, res) => {
-  try {
-    const { username, email, password } = req.body;
+const register = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
 
-    //! Validate
-    if (!username || !email || !password) {
-      return res
-        .status(400)
-        .json({ message: "Please all fields are required" });
-    }
-    // Check if user already exists
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return res.status(400).json({ message: "Email already exists" });
-    }
-    // Hash the password
-    const hashedPassword = await bcrypt.hash(password, 10);
-    // Create a new user
-    const user = new User({
+  // Validate
+  if (!username || !email || !password) {
+    res.status(400);
+    throw new Error("Please all fields are required");
+  }
+
+  // Check the email is taken
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  // Hash the user password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create the user
+  const newUser = new User({
+    username,
+    password: hashedPassword,
+    email,
+  });
+
+  // Add the date the trial will end
+  newUser.trialExpires = new Date(
+    new Date().getTime() + newUser.trialPeriod * 24 * 60 * 60 * 1000
+  );
+
+  // Save the user
+  await newUser.save();
+  res.json({
+    status: true,
+    message: "Registration was successful",
+    user: {
       username,
       email,
-      password: hashedPassword,
-    });
-    // Save the user
-    await user.save();
-    // Return success message
-    res.json({
-      status: true,
-      message: "Registration was successful",
-    });
-  } catch (err) {}
-};
+    },
+  });
+});
+
 //--------- Login ---------
 //--------- Logout ---------
 //--------- Profile ---------
---
+++ backend/utils/ConnectDB.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
