cec70c2848937ff3bd03e182032f5fd449bb1e71
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-13 20:57:00 +0530

    MERN - ChatGenX Project - Backend - 31

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -1,16 +1,27 @@
 const asyncHandler = require("express-async-handler");
+const jwt = require("jsonwebtoken");
+const User = require("../models/User"); // Import the User model
 
 const isAuthenticated = asyncHandler(async (req, res, next) => {
-  if (req.cookies.token) {
-    //! VERIFY THE TOKEN
-    const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
-    // ADD THE USER TO THE REQ OBJ
-    req.user = await User.findById(decoded?.id).select("-password");
-    return;
-  } else {
-    return res.status(401).json({ message: "Not authorized" });
+  try {
+    if (req.cookies.token) {
+      // VERIFY THE TOKEN
+      const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+
+      // ADD THE USER TO THE REQ OBJ
+      req.user = await User.findById(decoded?.id).select("-password");
+
+      if (!req.user) {
+        return res.status(401).json({ message: "User not found" });
+      }
+
+      return next();
+    } else {
+      return res.status(401).json({ message: "Not authorized, no token" });
+    }
+  } catch (error) {
+    return res.status(401).json({ message: "Not authorized, token failed" });
   }
-  next();
 });
 
 module.exports = isAuthenticated;
