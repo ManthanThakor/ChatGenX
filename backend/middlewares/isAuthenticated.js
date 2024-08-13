5080e47aa24886dd539a579a8b44f6a41b8c2064
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-13 20:53:34 +0530

    MERN - ChatGenX Project - Backend - 30

--- backend/middlewares/isAuthenticated.js
+++ backend/middlewares/isAuthenticated.js
@@ -3,9 +3,12 @@ const asyncHandler = require("express-async-handler");
 const isAuthenticated = asyncHandler(async (req, res, next) => {
   if (req.cookies.token) {
     //! VERIFY THE TOKEN
-    const decoded = jwt.verify();
+    const decoded = jwt.verify(req.cookies.token, process.env.JWT_SECRET);
+    // ADD THE USER TO THE REQ OBJ
+    req.user = await User.findById(decoded?.id).select("-password");
+    return;
   } else {
-    return res.status(401).json({ message: "Not authenticated" });
+    return res.status(401).json({ message: "Not authorized" });
   }
   next();
 });
