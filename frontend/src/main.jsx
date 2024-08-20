37336f0abfbc39615bcc4b5e72e18094993388d6
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-20 14:49:49 +0530

    Mern- chatgenx frontend 22

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+// Create a new instance of QueryClient
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")).render(
+  //! React Query Client
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  </StrictMode>
+);
