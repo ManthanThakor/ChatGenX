44cfc69236398aab89bf40390ce88fdff8c06ceb
Manthan Thakor <thakormanthan849@gmail.com>
2024-08-20 15:13:24 +0530

    Mern- chatgenx frontend 25

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Home from "./components/Home/Home";
 import AboutUs from "./components/About/AboutUs";
+import Registration from "./components/Users/Register";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<AboutUs />} />
+        <Route path="/register" element={<Registration />} />
+        <Route path="*" element={() => <h1>Page not found</h1>} />
       </Routes>
     </BrowserRouter>
   );
